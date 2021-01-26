b85a337bff2dc92f9c82f11571a172346a456c5f
Hitomi <hitomi.winberg@gmail.com>
2021-01-26 14:15:13 +0100

    Uppdate to first step
    Create new router and delete hello world

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/create-new-todo',
+    name: 'CreateNewTodo',
+    component: () => import(/* webpackChunkName: "create" */ '../views/CreateNewTodo.vue')
   }
 ]
 
